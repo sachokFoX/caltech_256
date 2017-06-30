9703e860558ef6374883a3a956e1502948328cd8
Bohdan Sachkovskyi <bogdan.sachkovskyi@gmail.com>
2017-06-30 22:52:19 +0300

    rework dataset reading in neural network

--- classification/neural_network.py
+++ classification/neural_network.py
@@ -5,29 +5,26 @@ import keras
 import random
 import numpy as np
 from scipy import misc
-from typing import List,Tuple
+from typing import List, Tuple, Dict
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation
+from preprocessing.images_reader import ImagesReader
+
 
 def run(size: Tuple[int, int], dataset_dir: str):
-    epochs = 30
+    epochs = 100
     batch_size = 128
     classes_count = 257
     input_shape = size[0] * size[1] * 3
 
-    (x_train, y_train) = __read_dataset__(size, os.path.join(dataset_dir, 'train'))
-    (x_test, y_test) = __read_dataset__(size, os.path.join(dataset_dir, 'test'))
-    (x_validation, y_validation) = __read_validation_dataset__(size, os.path.join(dataset_dir, 'validate'))
+    (x_train, y_train) = __read_train_dataset__(size, dataset_dir)
+    (x_test, y_test) = __read_test_dataset__(size, dataset_dir)
 
     y_train = keras.utils.to_categorical(y_train, classes_count)
-    y_test = keras.utils.to_categorical(y_test, classes_count)
 
     print('*****************************')
     print('x_train shape:', x_train.shape)
-    print('y_train shape:', y_train.shape)
     print('x_test shape:', x_test.shape)
-    print('y_test shape:', y_test.shape)
-    print('x_validation shape:', x_validation.shape)
     print('*****************************')
 
     model = Sequential()
@@ -36,73 +33,77 @@ def run(size: Tuple[int, int], dataset_dir: str):
     model.add(Activation('relu'))
     model.add(Dropout(0.5))
 
-    for i in range(0, 5):
-        model.add(Dense(512))
+    for i in range(0, 2):
+        model.add(Dense(1024))
         model.add(Activation('relu'))
 
-    model.add(Dropout(0.5))
+    model.add(Dropout(0.25))
 
     model.add(Dense(classes_count))
     model.add(Activation('softmax'))
 
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-    earlyStopping = keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0.02, patience=2, verbose=1, mode='auto')
-    tensorBoard = keras.callbacks.TensorBoard(log_dir=os.path.join('./logs', str(time.time())), write_graph=True, write_images=True)
+    earlyStopping = keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0.002, patience=3, verbose=1, mode='auto')
+    tensorBoard = keras.callbacks.TensorBoard(log_dir=os.path.join('./logs', str(time.time())))
 
     model.fit(
         x_train,
         y_train,
         batch_size=batch_size,
         epochs=epochs,
-        validation_data=(x_test, y_test),
-        callbacks=[earlyStopping, tensorBoard])
+        validation_split=0.2,
+        callbacks=[tensorBoard])
 
-    # test_result = model.evaluate(x_test, y_test, batch_size=batch_size)
+    prediction_result = model.predict_classes(x_test) + 1
 
-    prediction_result = model.predict_classes(x_validation) + 1
-
-    # print()
-    # print('Test score:', test_result[0])
-    # print('Test accuracy:', test_result[1])
-
+    print()
     print('Saving results')
     with open('nn_results.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
         writer.writerow(['image', 'class'])
-        for i in range(0, len(x_validation)):
-            writer.writerow([y_validation[i], prediction_result[i]])
+        for i in range(0, len(x_test)):
+            writer.writerow([y_test[i], prediction_result[i]])
 
-def __read_dataset__(size: Tuple[int, int], dataset_dir: str) -> Tuple[List[List[int]], List[int]]:
-    images = [img for img in os.listdir(dataset_dir) if img.endswith('.jpg')]
+
+def __read_train_dataset__(size: Tuple[int, int], dataset_dir: str) -> Tuple[np.ndarray, np.ndarray]:
+    reader = ImagesReader(dataset_dir)
+    images = reader.read_train_images()
+
+    flatten_images = []
+    for image_class in images:
+        for image in images[image_class]:
+            flatten_images.append((int(image_class), image.path))
+    random.shuffle(flatten_images)
+
+    images_count = len(flatten_images)
+
+    x = np.empty(shape=(images_count, size[0] * size[1] * 3), dtype=np.float32)
+    y = np.empty(shape=(images_count,), dtype=np.int16)
+
+    for (i, (image_class, image_path)) in enumerate(flatten_images):
+        x[i, :] = __get_np_image__(image_path)
+        y[i] = image_class - 1
+
+    return x, y
+
+
+def __read_test_dataset__(size: Tuple[int, int], dataset_dir: str) -> Tuple[List[List[int]], List[str]]:
+    reader = ImagesReader(dataset_dir)
+    images = reader.read_test_images()
     random.shuffle(images)
+
     images_count = len(images)
 
     x = np.empty(shape=(images_count, size[0] * size[1] * 3), dtype=np.float32)
-    y = np.empty(shape=(images_count, ), dtype=np.int16)
+    y = [None] * images_count
 
-    for i, image in enumerate(images):
-        x[i, :] = __get_np_image__(os.path.join(dataset_dir, image))
-        y[i] = __get_class_id__(image)
-        # print(image)
+    for i, image_metadata in enumerate(images):
+        x[i, :] = __get_np_image__(image_metadata.path)
+        y[i] = image_metadata.file_name
 
-    return (x, y)
+    return x, y
 
-def __read_validation_dataset__(size: Tuple[int, int], dataset_dir: str) -> Tuple[List[List[int]], List[str]]:
-    images = [img for img in os.listdir(dataset_dir) if img.endswith('.jpg')]
-    images_count = len(images)
-
-    x = np.empty(shape=(images_count, size[0] * size[1] * 3), dtype=np.float32)
-    y = []
-
-    for i, image in enumerate(images):
-        x[i, :] = __get_np_image__(os.path.join(dataset_dir, image))
-        y.append(image)
-
-    return (x, y)
 
 def __get_np_image__(image_path: str) -> np.ndarray:
     return misc.imread(image_path).flatten() / 255
-
-def __get_class_id__(image_name: str) -> str:
-    return int(image_name.split('_')[0]) - 1
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import time
 from termcolor import cprint
 from preprocessing.images_reader import ImagesReader
 from preprocessing.preprocessor import PreProcessor
-# import classification.neural_network as nn
+import classification.neural_network as nn
 
 start_time = time.time()
 
@@ -12,17 +12,17 @@ base_dir = '/Users/bohdan/work/uds-club'
 original_dataset_dir = os.path.join(base_dir, 'dataset_caltech_256')
 preprocessed_dataset_dir = os.path.join(base_dir, 'dataset_caltech_256_preprocessed')
 
-cprint('reading images...', 'green')
-reader = ImagesReader(original_dataset_dir)
+# cprint('reading images...', 'green')
+# reader = ImagesReader(original_dataset_dir)
+#
+# train_images = reader.read_train_images()
+# test_images = reader.read_test_images()
+#
+# cprint('preprocessing images...', 'green')
+# preprocessor = PreProcessor(preprocessed_dataset_dir)
+# preprocessor.preprocess(train_images, test_images, size)
 
-train_images = reader.read_train_images()
-test_images = reader.read_test_images()
-
-cprint('preprocessing images...', 'green')
-preprocessor = PreProcessor(preprocessed_dataset_dir)
-preprocessor.preprocess(train_images, test_images, size)
-
-# nn.run(size, preprocessed_dataset_dir)
+nn.run(size, preprocessed_dataset_dir)
 
 end_time = time.time()
 cprint('done in {:.2f}s'.format(end_time - start_time), 'green')
